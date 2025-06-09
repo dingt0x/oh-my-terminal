488bd07c308f9a82554c6a23d385e5e5c54d710b
dingtianwei <dingtianwei@gmail.com>
2025-06-09 17:39:07 +0800

    fix add route

--- entry/add_route.sh
+++ entry/add_route.sh
@@ -9,18 +9,24 @@ add_route(){
     echo "$ROUTE" | while read -r route_items; do
         dest=${route_items%%,*}
         gw=${route_items##*,}
+        echo "dest $dest" 
+        echo "gw $gw" 
+        # Ignore empty lines or comments
         if [ "${dest:0:1}" = "" ] || [ "${dest:0:1}" = "#" ]; then
-          continue
-        fi
-
-        if ip route | grep -c "$dest" > /dev/null 2>&1; then
-          continue
-        fi
-
-        if (ip addr |grep "${gw%.*}" > /dev/null 2>&1); then
             continue
         fi
 
+        # Ignore if existing
+        if ip route | grep -c "$dest" > /dev/null 2>&1; then
+            continue
+        fi
+
+        # Ignore if no subnet
+        if ! (ip addr |grep "${gw%.*}" > /dev/null 2>&1); then
+            continue
+        fi
+
+        # Ignore if gw is self
         if  (ip addr |grep "${gw}" > /dev/null 2>&1); then
             continue
         fi
