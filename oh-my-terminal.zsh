ccc2364d44a65db0d550aaf8dc2883dc8385e6b5
dingtianwei <dingtianwei@gmail.com>
2025-07-02 17:54:29 +0800

    update

--- oh-my-terminal.zsh
+++ oh-my-terminal.zsh
@@ -5,123 +5,14 @@ if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
     exit 1
 fi
 
-
 if [ -z "$OMT" ]; then
     printf "\033[31m%s\033[0m\n" "OMT NOT FOUND"
     return
 fi
 
-
-[[ -n "$OMT_CUSTOM" ]] || OMT_CUSTOM="$OMT/custom"
-[[ -n "$OMT_CACHE_DIR" ]] || OMT_CACHE_DIR="$HOME/.cache/oh-my-terminal"
-[[ -d "$OMT_CACHE_DIR/completions" ]] || mkdir -p "$OMT_CACHE_DIR/completions"
+[ -f "$OMT/lib/oh-my-zsh.zsh" ] &&  source "$OMT/lib/oh-my-zsh.zsh"
 
 
-fpath=($OMT/{functions,completions} $OMT_CUSTOM/{functions,completions} $fpath)
-
-
-
-autoload -U compaudit compinit zrecompile
-
-is_plugin() {
-  local base_dir=$1
-  local name=$2
-  builtin test -f $base_dir/plugins/$name/$name.plugin.zsh \
-    || builtin test -f $base_dir/plugt ins/$name/_$name
-}
-
-for plugin in "${plugins[@]}"; do
-  if is_plugin "$OMT_CUSTOM" "$plugin"; then
-    fpath=("$OMT_CUSTOM/plugins/$plugin" $fpath)
-  elif is_plugin "$OMT" "$plugin"; then
-    fpath=("$OMT/plugins/$plugin" $fpath)
-  else
-    echo "[oh-my-zsh] plugin '$plugin' not found"
-  fi
-done
-
-SHORT_HOST="dingt0x-omt"
-ZSH_COMPDUMP="${HOME}/.cache/.zcompdump-${SHORT_HOST}-20250701"
-
-zcompdump_revision="#omz revision: $(builtin cd -q "$OMT"; git rev-parse HEAD 2>/dev/null)"
-zcompdump_fpath="#omz fpath: $fpath"
-
-# Delete the zcompdump file if OMZ zcompdump metadata changed
-if ! command grep -q -Fx "$zcompdump_revision" "$ZSH_COMPDUMP" 2>/dev/null \
-   || ! command grep -q -Fx "$zcompdump_fpath" "$ZSH_COMPDUMP" 2>/dev/null; then
-  command rm -f "$ZSH_COMPDUMP"
-  zcompdump_refresh=1
-fi
-
-
-if [[ "$ZSH_DISABLE_COMPFIX" != true ]]; then
-  source "$OMT/lib/compfix.zsh"
-  # Load only from secure directories
-  compinit -i -d "$ZSH_COMPDUMP"
-  # If completion insecurities exist, warn the user
-  handle_completion_insecurities &|
-else
-  # If the user wants it, load from all found directories
-  compinit -u -d "$ZSH_COMPDUMP"
-fi
-
-if (( $zcompdump_refresh )) \
-  || ! command grep -q -Fx "$zcompdump_revision" "$ZSH_COMPDUMP" 2>/dev/null; then
-  # Use `tee` in case the $ZSH_COMPDUMP filename is invalid, to silence the error
-  # See https://github.com/ohmyzsh/ohmyzsh/commit/dd1a7269#commitcomment-39003489
-  tee -a "$ZSH_COMPDUMP" &>/dev/null <<EOF
-
-$zcompdump_revision
-$zcompdump_fpath
-EOF
-fi
-
-unset zcompdump_revision zcompdump_fpath zcompdump_refresh
-
-# zcompile the completion dump file if the .zwc is older or missing.
-if command mkdir "${ZSH_COMPDUMP}.lock" 2>/dev/null; then
-  zrecompile -q -p "$ZSH_COMPDUMP"
-  command rm -rf "$ZSH_COMPDUMP.zwc.old" "${ZSH_COMPDUMP}.lock"
-fi
-
-
-
-
-for plugin in "${plugins[@]}"; do
-  if is_plugin "$OMT_CUSTOM" "$plugin"; then
-     source "$OMT_CUSTOM/plugins/$plugin/${plugin}.plugin.zsh"
-  elif is_plugin "$OMT" "$plugin"; then
-     source "$OMT/plugins/${plugin}/${plugin}.plugin.zsh"
-  fi
-done
-unset plugin
-
-
-
-
-# Load the theme
-is_theme() {
-  local base_dir=$1
-  local name=$2
-  builtin test -f "$base_dir/$name.zsh-theme"
-}
-
-OMT_THEME=${ZSH_THEME:-"dingt0xavit"}
-
-if [[ -n "$OMT_THEME" ]]; then
-  if is_theme "$OMT_CUSTOM" "$OMT_THEME"; then
-    # shellcheck disable=SC1090
-    source "$OMT_CUSTOM/$OMT_THEME.zsh-theme"
-  elif is_theme "$OMT_CUSTOM/themes" "$OMT_THEME"; then
-    # shellcheck disable=SC1090
-    source "$OMT_CUSTOM/themes/$OMT_THEME.zsh-theme"
-  elif is_theme "$OMT/themes" "$OMT_THEME"; then
-    # shellcheck disable=SC1090
-    source "$OMT/themes/$OMT_THEME.zsh-theme"
-  else
-    echo "[oh-my-zsh] theme '$OMT_THEME' not found"
-  fi
-fi
 
 load_files="
 ${OMT}/load/_load_path.sh
@@ -136,6 +27,7 @@ echo "$load_files" | while read -r load_file; do
         source "$load_file"
     fi
 done
+
 unset load_file
 unset load_files
 
