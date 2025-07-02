a4487360a7ef4b5e5b20341b9ad61aa211339686
dingtianwei <dingtianwei@gmail.com>
2025-07-02 13:35:11 +0800

    update directories.zsh color

--- lib/directories.zsh
+++ lib/directories.zsh
@@ -34,7 +34,14 @@ function d () {
 compdef _dirs d
 
 # List directory contents
-alias lsa='ls -lah'
-alias l='ls -lah'
-alias ll='ls -lh'
-alias la='ls -lAh'
+if ls --color >/dev/null 2>&1; then  # GNU ls
+  alias lsa='ls -lah --color=auto'
+  alias l='ls -lah --color=auto'
+  alias ll='ls -lh --color=auto'
+  alias la='ls -lAh --color=auto'
+else  # macOS ls
+  alias lsa='ls -lah'
+  alias l='ls -lah'
+  alias ll='ls -lh'
+  alias la='ls -lAh'
+fi
